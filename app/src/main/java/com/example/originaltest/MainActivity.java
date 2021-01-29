8ab3bbe9a0762e58c47e3719d0934f5956bfb6c7
dd <shch777@naver.com>
2021-01-29 12:26:13 +0900

    2021-01-29

--- app/src/main/java/com/example/originaltest/MainActivity.java
+++ app/src/main/java/com/example/originaltest/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.originaltest;
 
 import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     Fragment record;
     Fragment profile;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,27 +33,29 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.container, routine).commit();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom);
+        // 네비게이션 뷰 아이콘 색상
+        //bottomNavigationView.setItemIconTintList(null);
+
         bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch(item.getItemId()) {
-                            case R.id.tab_routine:
-                                getSupportFragmentManager().beginTransaction().replace(R.id.container,routine).commit();
-                                return true;
-                            case R.id.tab_exercise:
-                                getSupportFragmentManager().beginTransaction().replace(R.id.container,exercise).commit();
-                                return true;
-                            case R.id.tab_record:
-                                getSupportFragmentManager().beginTransaction().replace(R.id.container,record).commit();
-                                return true;
-                            case R.id.tab_profile:
-                                getSupportFragmentManager().beginTransaction().replace(R.id.container,profile).commit();
-                                return true;
-                        }
-                        return false;
+                item -> {
+                    switch(item.getItemId()) {
+                        case R.id.tab_routine:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container,routine).commit();
+                            return true;
+                        case R.id.tab_exercise:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container,exercise).commit();
+                            return true;
+                        case R.id.tab_record:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container,record).commit();
+                            return true;
+                        case R.id.tab_profile:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container,profile).commit();
+                            return true;
                     }
+                    return false;
                 }
         );
+
     }
+
 }
