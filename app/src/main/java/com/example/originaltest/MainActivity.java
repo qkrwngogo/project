8f99542c82ad3bbd383173b196cc4cd7b2ab7eb2
dd <shch777@naver.com>
2021-02-01 10:09:16 +0900

    2021-02-01

--- app/src/main/java/com/example/originaltest/MainActivity.java
+++ app/src/main/java/com/example/originaltest/MainActivity.java
@@ -2,28 +2,63 @@ package com.example.originaltest;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import nl.joery.animatedbottombar.AnimatedBottomBar;
 
 
-public class MainActivity extends FragmentActivity {
-
+public class MainActivity extends AppCompatActivity {
+    FragmentPagerAdapter adapterViewPager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ViewPager2 viewPager2 = findViewById(R.id.viewPager2_container);
-        AnimatedBottomBar animatedBottomBar = findViewById(R.id.bottom_bar);
-
-
+        ViewPager vpPager = (ViewPager) findViewById(R.id.viewPager2_container);
+        adapterViewPager = new MyPagerAdapter((getSupportFragmentManager()));
+        vpPager.setAdapter(adapterViewPager);
+        AnimatedBottomBar bottomBar = (AnimatedBottomBar) findViewById(R.id.bottom_bar);
+        bottomBar.setupWithViewPager(vpPager);
     }
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 2;
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
 
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return Routine.newInstance(0,"Routine");
+                case 1:
+                    return Exercise.newInstance(1,"page 2");
+                default:
+                    return null;
+            }
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Page" + position;
+        }
+    }
 
 }
 
--- app/src/main/java/com/example/originaltest/Routine.java
+++ app/src/main/java/com/example/originaltest/Routine.java
@@ -10,14 +10,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class Exercise extends Fragment {
+public class Routine extends Fragment {
     private String title;
     private int page;
 
     public static Routine newInstance (int page, String title) {
         Routine routine = new Routine();
         Bundle args = new Bundle();
-        args.putInt("1",page);
+        args.putInt("0",page);
         args.putString("someTitle",title);
         routine.setArguments(args);
         return routine;
@@ -26,14 +26,14 @@ public class Exercise extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        page = getArguments().getInt("1",1);
+        page = getArguments().getInt("0",0);
         title = getArguments().getString("someTitle");
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_exercise, container, false);
+        View view = inflater.inflate(R.layout.fragment_routine, container, false);
         return view;
     }
 }
